8d424b2bc101bbd32e5d107dbafb4bd563a8e790
NankinRam <vadimnankin12@gmail.com>
2025-06-21 21:02:21 +0300

    Начало

---
+++ src/main/java/org/example/educationprojectrest/EducationProjectRestApplication.java
@@ -0,0 +1,13 @@
+package org.example.educationprojectrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationProjectRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationProjectRestApplication.class, args);
+    }
+
+}
